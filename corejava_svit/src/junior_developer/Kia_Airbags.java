6723fd0a930e9c3c42ec2566cfd9fa01090d1d8f
16swathii <157670862+16swathii@users.noreply.github.com>
2025-08-07 02:28:02 -0400

    Third commit

---
+++ corejava_svit/src/junior_developer/Kia_Airbags.java
@@ -0,0 +1,8 @@
+package junior_developer;
+
+public class Kia_Airbags {
+	public void disp_airbags()
+	{
+		System.out.println("All Kia have airbags");
+	}
+}
---
+++ corejava_svit/src/junior_developer/Kia_internal.java
@@ -0,0 +1,10 @@
+package junior_developer;
+
+public class Kia_internal {
+	public static void main(String[] args) {
+		junior_developer.Kia_Airbags obj=new junior_developer.Kia_Airbags();
+		obj.disp_airbags();
+		junior_developer.Kia_speedlimit obj1=new junior_developer.Kia_speedlimit();
+		obj1.disp_speedlimit();
+	}
+}
