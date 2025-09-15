7329369f7dbfd7101ffb17dda9bc4fe7dc393cff
16swathii <157670862+16swathii@users.noreply.github.com>
2025-09-15 07:31:04 -0400

    first commit

---
+++ corejava_svit/src/exception_demo/Errors_demo.java
@@ -0,0 +1,17 @@
+package exception_demo;
+
+public class Errors_demo {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
+	
+	}
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+
+}
